0859b20b19f9b7737ce2074bca823725a201349c
AbyelT <abielo.tesfay@gmail.com>
2023-01-05 16:47:27 +0100

    add titanic

--- Project/api_data.py
+++ Project/api_data.py
@@ -9,11 +9,11 @@ start = pd.Timestamp('20220101', tz='Europe/Brussels')
 end = pd.Timestamp('20221231', tz='Europe/Brussels')
 country_code = 'SE'  
 
-# Generation per type
-client.query_generation(country_code, start=start,end=end, psr_type=None)
+# # Generation per type
+# client.query_generation(country_code, start=start,end=end, psr_type=None)
 
-# Day-ahead price
-client.query_day_ahead_prices(country_code, start=start,end=end)
+# # Day-ahead price
+# client.query_day_ahead_prices(country_code, start=start,end=end)
 
-# Actual load (consumption)
-client.query_load(country_code, start=start,end=end)
+# # Actual load (consumption)
+# client.query_load(country_code, start=start,end=end)
